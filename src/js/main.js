3dc6c90f746b7510c6704a9d8878ed149f21358a
Felipe Bernardes <bernardes.flp@gmail.com>
2020-07-03 10:59:13 -0300

    Initialize firebase real time db

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,14 @@
 import '../scss/main.scss';
+
 const firebase = require('firebase/app');
-const database = require('firebase/database');
+require('firebase/database');
 
 //TODO set apikeys as env variables
 const app = firebase.initializeApp({
-  databaseURL: 'https://hacker-news.firebaseio.com/v0'
+  databaseURL: 'https://hacker-news.firebaseio.com'
 });
 
 const db = firebase.database(app);
 
-console.log('heyehye');
-console.log('test', db, app);
-console.log('test', db, app);
+console.log('db', db);
+console.log('app', app);
