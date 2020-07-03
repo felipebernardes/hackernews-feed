3ffd7fa5b9bffc68a87971cca8fc6349eda1ece2
Felipe Bernardes <bernardes.flp@gmail.com>
2020-07-03 11:22:22 -0300

    Add API real time use case sketch

--- src/js/main.js
+++ src/js/main.js
@@ -12,3 +12,21 @@ const db = firebase.database(app);
 
 console.log('db', db);
 console.log('app', app);
+
+
+const newStoriesRef = db.ref('/v0/newstories');
+newStoriesRef.on('value', function(snapshot) {
+  const newStoriesIds = snapshot.val();
+  console.log('newStoriesIds', newStoriesIds);
+  retrievePost(newStoriesIds[0]);
+});
+
+const retrievePost = (postId) => {
+  const postRef = db.ref(`/v0/item/${postId}`);
+  
+  postRef.once('value', function(snapshot) {
+    const post = snapshot.val();
+    console.log('post', post);
+  })
+}
+
