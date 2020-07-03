b045b68df2886c94756c80f79836ce342d2e42cc
Felipe Bernardes <bernardes.flp@gmail.com>
2020-07-03 14:26:08 -0300

    Handle first render and newer posts appearing, sketch infinite scroll

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 import '../scss/main.scss';
+import renderPost from './components/post';
 
 const firebase = require('firebase/app');
 require('firebase/database');
@@ -7,38 +8,78 @@ require('firebase/database');
 const app = firebase.initializeApp({
   databaseURL: 'https://hacker-news.firebaseio.com'
 });
-
 const db = firebase.database(app);
-
-console.log('db', db);
-console.log('app', app);
-
-
 const newStoriesRef = db.ref('/v0/newstories');
 
-newStoriesRef.once('value', async function(snapshot) {
+const postsPerIteration = 30;
+const postList = document.querySelector('[data-post-list]');
+var firstPostId, lastPostId;
+let currentPage = 1;
+
+newStoriesRef.on('value', function(snapshot) {
   const newStoriesIds = snapshot.val();
   console.log('newStoriesIds', newStoriesIds);
 
-  newStoriesIds.slice(0,5).forEach(async (storyId) => {
+  // NEW DATA
+  if (firstPostId && lastPostId) {
+    console.log('new posts!');
+    const diff = newStoriesIds.filter(id => id > firstPostId);
+    handleNewPosts(diff);
+  }
+
+  // FIRST RENDER
+  if (!firstPostId && !lastPostId) {
+    // TODO save these in localstorage
+    console.log('first render!');
+    firstPostId = newStoriesIds[0];
+    lastPostId = newStoriesIds[499];
+
+    renderPosts(newStoriesIds, currentPage);
+  }
+});
+
+const handleNewPosts = (newPostsIds) => {
+  newPostsIds.forEach(async (storyId) => {
     const postRef = await retrievePostRef(storyId);
-
-    postRef.once('value', async function(snapshot) {
-      const postData = snapshot.val();
-      console.log('post', postData);
-  
-      const postMarkup = `
-        <li>
-          <h2><a href="${postData.url}">${postData.title}</a></h2>
-          <date>${postData.time}</date>
-          <p>by ${postData.by}</p>
-        </li>
-      `;
-
-      document.querySelector('ul').innerHTML += postMarkup;
+    
+    postRef.once('value', async (snapshot) => {
+      const postData = await snapshot.val();
+      
+      if (postData) {
+        firstPostId = storyId;
+        console.log('latest', postData.title);
+        console.log('latest', storyId);
+        renderPost(postData, postList, 'top');
+      }
     });
   });
-});
+}
+
+window.onscroll = function(ev) {
+  if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+      console.log('scrolled to bottom!');
+      //detach event listener
+      //load more items from the original list
+      //smooth scroll
+      //reattach event listener
+  }
+};
+
+const renderPosts = (postIds, page) => {
+  postIds.slice(0, page * postsPerIteration).forEach(async (storyId, idx) => {
+    const postRef = await retrievePostRef(storyId);
+
+    postRef.once('value', async (snapshot) => {
+      const postData = await snapshot.val();
+
+      if (postData) {
+        setTimeout(() => {
+          renderPost(postData, postList, 'bottom');
+        }, 130 * idx+1);
+      }
+    });
+  });
+}
 
 const retrievePostRef = async (postId) => {
   const postRef = await db.ref(`/v0/item/${postId}`);
