d92f7bdd23e7cad597e06c92d20b31720fbd3b6f
Felipe Bernardes <bernardes.flp@gmail.com>
2020-07-03 12:38:01 -0300

    Populate feed on initial load

--- src/js/main.js
+++ src/js/main.js
@@ -15,18 +15,33 @@ console.log('app', app);
 
 
 const newStoriesRef = db.ref('/v0/newstories');
-newStoriesRef.on('value', function(snapshot) {
+
+newStoriesRef.once('value', async function(snapshot) {
   const newStoriesIds = snapshot.val();
   console.log('newStoriesIds', newStoriesIds);
-  retrievePost(newStoriesIds[0]);
+
+  newStoriesIds.slice(0,5).forEach(async (storyId) => {
+    const postRef = await retrievePostRef(storyId);
+
+    postRef.once('value', async function(snapshot) {
+      const postData = snapshot.val();
+      console.log('post', postData);
+  
+      const postMarkup = `
+        <li>
+          <h2><a href="${postData.url}">${postData.title}</a></h2>
+          <date>${postData.time}</date>
+          <p>by ${postData.by}</p>
+        </li>
+      `;
+
+      document.querySelector('ul').innerHTML += postMarkup;
+    });
+  });
 });
 
-const retrievePost = (postId) => {
-  const postRef = db.ref(`/v0/item/${postId}`);
-  
-  postRef.once('value', function(snapshot) {
-    const post = snapshot.val();
-    console.log('post', post);
-  })
+const retrievePostRef = async (postId) => {
+  const postRef = await db.ref(`/v0/item/${postId}`);
+  return postRef;
 }
 
