f9fc89a25cf7435111a89334d3a74597ac12b816
Felipe Bernardes <bernardes.flp@gmail.com>
2020-07-04 16:53:49 -0300

    Update post list items to link both to HN story and post URL

--- src/js/actions/renderPost.js
+++ src/js/actions/renderPost.js
@@ -5,7 +5,10 @@ const formatTimestamp = (timestamp) => {
 const generateMarkup = (postData) => {
   return `
     <li class="post-list__item">
-      <h2 class="post-list__item__title"><a href="${postData.url}">${postData.title} (${postData.url})</a></h2>
+      <h2 class="post-list__item__title">
+        <a href="https://news.ycombinator.com/item?id=${postData.id}">${postData.title}</a>
+        &nbsp;<a href="${postData.url}">(${postData.url})<a>
+        </h2>
       <time class="post-list__item__date">${formatTimestamp(postData.time)}</time>
       <p class="post-list__item__author">by ${postData.by}</p>
       <p class="post-list__item__author">id ${postData.id}</p>
@@ -15,6 +18,8 @@ const generateMarkup = (postData) => {
 
 const renderPost = (postData, postList, placement = 'bottom') => {
   const markup = generateMarkup(postData);
+
+  console.log('postData', postData);
 
   if (placement === 'bottom') {
     postList.insertAdjacentHTML('beforeend', markup);
