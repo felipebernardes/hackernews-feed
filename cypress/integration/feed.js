57fd63819c8166783f8239232df3a9643bd4ae0c
Felipe Bernardes <bernardes.flp@gmail.com>
2020-07-06 00:38:42 -0300

    Add cypress and feed integration test

---
+++ cypress/integration/feed.js
@@ -0,0 +1,19 @@
+describe('HackerNews Feed', () => {
+  it('Access HackerNews Feed', () => {
+    cy.visit('/');
+  });
+
+  it('Should contain post list', () => {
+    cy.get('ul[data-post-list]').should('exist');
+  });
+
+  it('Should have 10 posts initially', () => {
+    cy.get('ul[data-post-list] li').should('have.length', 10);
+  });
+
+  it('Should load 10 more posts after we scroll to bottom', () => {
+    cy.scrollTo('bottom');
+    cy.wait(1000);
+    cy.get('ul[data-post-list] li').should('have.length', 20);
+  })
+});
